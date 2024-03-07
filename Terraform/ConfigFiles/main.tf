648787d3b65ed54aecb1186f3bedb5bb3b13235a
Theerej <theerej@gmail.com>
2024-03-07 11:47:49 +0530

    vault backup: 2024-03-07 11:47:49

--- Terraform/ConfigFiles/main.tf
+++ Terraform/ConfigFiles/main.tf
@@ -1,21 +1,39 @@
 provider "aws" {
-    region = "us-east-1"
-    access_key = "test123"
-    secret_key = "test"
-    skip_credentials_validation = true
-    skip_requesting_account_id = true
-    skip_metadata_api_check = true
-    s3_use_path_style = true
-    endpoints {
-      s3 = "http://localhost:4566"
-      ec2 = "http://localhost:4566"
-    }
-    
+  region                      = "us-east-1"
+  access_key                  = "test123"
+  secret_key                  = "test"
+  skip_credentials_validation = true
+  skip_requesting_account_id  = true
+  skip_metadata_api_check     = true
+  s3_use_path_style           = true
+  endpoints {
+    s3  = "http://localhost:4566"
+    ec2 = "http://localhost:4566"
+  }
+
 }
 resource "aws_s3_bucket" "b" {
-    bucket = "theerej1"
+  bucket = "theerej1"
 }
 resource "aws_instance" "example" {
-ami = "ami-0fb653ca2d3203ac1"
-instance_type = "t2.micro"
+  ami                         = "ami-0fb653ca2d3203ac1"
+  instance_type               = "t2.micro"
+  user_data                   = <<-EOF
+            #!/bin/bash
+            echo "Hello, World" > index.xhtml
+            nohup busybox httpd -f -p 8080 &
+            EOF
+  user_data_replace_on_change = true
+  tags = {
+    Name = "terraform-example"
+  }
+}
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
