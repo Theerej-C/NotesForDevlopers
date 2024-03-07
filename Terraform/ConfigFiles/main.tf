e2489c09e62a5509add0075c04fa049c621c21e6
Theerej <theerej@gmail.com>
2024-03-07 12:12:49 +0530

    vault backup: 2024-03-07 12:12:49

--- Terraform/ConfigFiles/main.tf
+++ Terraform/ConfigFiles/main.tf
@@ -18,6 +18,7 @@ resource "aws_s3_bucket" "b" {
 resource "aws_instance" "example" {
   ami                         = "ami-0fb653ca2d3203ac1"
   instance_type               = "t2.micro"
+  vpc_security_group_ids      = [aws_security_group.instance.id]
   user_data                   = <<-EOF
             #!/bin/bash
             echo "Hello, World" > index.xhtml
