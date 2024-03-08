f288756b66291aa5231c962d95c90c64cfddf7b5
Theerej <theerej@gmail.com>
2024-03-08 10:50:06 +0530

    vault backup: 2024-03-08 10:50:06

--- Terraform/ConfigFiles/main.tf
+++ Terraform/ConfigFiles/main.tf
@@ -12,6 +12,11 @@ provider "aws" {
   }
 
 }
+variable "server_port" {
+  description = "The port the server will use for HTTP requests"
+  type        = number
+  default     = 8080
+}
 resource "aws_s3_bucket" "b" {
   bucket = "theerej1"
 }
@@ -22,7 +27,7 @@ resource "aws_instance" "example" {
   user_data                   = <<-EOF
             #!/bin/bash
             echo "Hello, World" > index.xhtml
-            nohup busybox httpd -f -p 8080 &
+            nohup busybox httpd -f -p ${var.server_port} &
             EOF
   user_data_replace_on_change = true
   tags = {
@@ -32,9 +37,13 @@ resource "aws_instance" "example" {
 resource "aws_security_group" "instance" {
   name = "terraform-example-instance"
   ingress {
-    from_port   = 8080
-    to_port     = 8080
+    from_port   = var.server_port
+    to_port     = var.server_port
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+}
+output "public_ip" {
+  value       = aws_instance.example.public_ip
+  description = "The public IP address of the web server"
 }
