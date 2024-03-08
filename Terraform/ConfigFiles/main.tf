3e2770e1b4b99ed156232d545b8340502be2d15f
Theerej <theerej@gmail.com>
2024-03-08 14:32:11 +0530

    vault backup: 2024-03-08 14:32:11

--- Terraform/ConfigFiles/main.tf
+++ Terraform/ConfigFiles/main.tf
@@ -47,37 +47,6 @@ output "public_ip" {
   value       = aws_instance.example.public_ip
   description = "The public IP address of the web server"
 }
-resource "aws_launch_configuration" "example" {
-  image_id        = "ami-0fb653ca2d3203ac1"
-  instance_type   = "t2.micro"
-  security_groups = [aws_security_group.instance.id]
-  user_data       = <<-EOF
-            #!/bin/bash
-            echo "Hello, World" > index.xhtml
-            nohup busybox httpd -f -p ${var.server_port} &
-            EOF
-  lifecycle {
-    create_before_destroy = true
-  }
-}
-resource "aws_autoscaling_group" "name" {
-  launch_configuration = aws_launch_configuration.example.name
-  min_size             = 2
-  max_size             = 10
-  vpc_zone_identifier  = data.aws_subnets.default.ids
-  tag {
-    key                 = "Name"
-    value               = "terraform-example"
-    propagate_at_launch = true
-  }
-}
-data "aws_subnets" "default" {
-  filter {
-    name   = "vpc-id"
-    values = [data.aws_vpc.default.id]
-  }
-}
-data "aws_vpc" "default" {
-  default = true
-}
+
+
 
